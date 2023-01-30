6baccc5d85c6b9fd22bed2a212b119967767c4a0
Alfurqani <syifa.alfurqoni@gmail.com>
2023-01-30 09:11:58 +0700

    add fish_greeting to disable fish welcome message

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -6,7 +6,5 @@ bind \t accept-autosuggestion
 bind X forward-word
 # bind E forward-word
 
-# $ bind | grep forward-char
-# bind \e\[C forward-char
-# bind -k right forward-char
-# bind \cf forward-char
+set -U fish_greeting
+
